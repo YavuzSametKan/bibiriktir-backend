c71679e0dac18ef7c1c488a6b7d51a779e152e71
yavuz samet kan <yssk.personal@gmail.com>
2025-03-23 17:23:52 +0300

    Unsafety sample auth files deleted.

--- src/features/auth/models.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose"
-
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    age: Number
-})
-
-export const User = mongoose.model('User', userSchema, 'users')
