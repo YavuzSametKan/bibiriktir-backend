ddb960663842a1fd5a7c542590187211937bceb8
yavuz samet kan <yssk.personal@gmail.com>
2025-05-24 05:03:30 +0300

    token generator updated

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -2,7 +2,8 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
-        expiresIn: '30d'
+        expiresIn: '30d',
+
     });
 };
 
